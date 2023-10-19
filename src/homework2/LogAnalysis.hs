fea544a8d243d811d74ba3d242976ad9da4dce43
PrimusAbOris <guy.ross.rit@gmail.com>
2023-10-19 11:27:32 -0400

    Create LogAnalysis.hs

---
+++ src/homework2/LogAnalysis.hs
@@ -0,0 +1,17 @@
+{-# OPTIONS_GHC -Wall #-}
+module LogAnalysis where
+
+import Log
+
+parseMessage :: String -> LogMessage
+    let inputString = ""
+    let wordList = words inputString
+
+-- I = info
+-- E = error
+-- W = warning
+
+
+
+main :: IO ()
+main = do
